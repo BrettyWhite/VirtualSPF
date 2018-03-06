a2a7edc4db90c8e56693051c3a861c18c0ad094b
Bretty White <geekman3454@protonmail.com>
2018-03-05 19:07:57 -0500

    update colors

--- VirtualSPF/Colors.swift
+++ VirtualSPF/Colors.swift
@@ -9,10 +9,10 @@
 import UIKit
 
 struct Colors {
-    static let Red    =      UIColor(cgColor: UIColor(rgba: "#ff0000").cgColor)
-    static let Orange =      UIColor(cgColor: UIColor(rgba: "#ff9900").cgColor)
-    static let Yellow =      UIColor(cgColor: UIColor(rgba: "#ffcc00").cgColor)
-    static let LightYellow = UIColor(cgColor: UIColor(rgba: "#ffff66").cgColor)
-    static let Green =       UIColor(cgColor: UIColor(rgba: "#1cd61c").cgColor)
+    static let Red    =      UIColor(cgColor: UIColor(rgba: "#fa5789").cgColor)
+    static let Orange =      UIColor(cgColor: UIColor(rgba: "#ff9c75").cgColor)
+    static let Yellow =      UIColor(cgColor: UIColor(rgba: "#fffa75").cgColor)
+    static let LightYellow = UIColor(cgColor: UIColor(rgba: "#daff75").cgColor)
+    static let Green =       UIColor(cgColor: UIColor(rgba: "#85ff75").cgColor)
     static let White =       UIColor(cgColor: UIColor(rgba: "#ffffff").cgColor)
 }
